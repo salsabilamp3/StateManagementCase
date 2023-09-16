79166630bf998d250a84ce2d64b257642736ebf4
salsabilamp3 <salsabilamp3@gmail.com>
2023-09-16 16:04:13 +0700

    Add result

---
+++ src/pages/UserPageDetail.js
@@ -0,0 +1,174 @@
+import { useLocation} from 'react-router-dom';
+import { useState } from 'react';
+import { sentenceCase } from 'change-case';
+import { Helmet } from 'react-helmet-async';
+// @mui
+import {
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  IconButton,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
+// components
+import Label from '../components/label';
+import Iconify from '../components/iconify';
+import Scrollbar from '../components/scrollbar';
+// sections
+import { UserDetailListHead } from '../sections/@dashboard/user';
+// mock
+import USERLIST from '../_mock/user';
+
+const TABLE_HEAD = [
+  { id: 'name', label: 'Name', alignRight: false },
+  { id: 'gender', label: 'Gender', alignRight: false },
+  { id: 'vehicle', label: 'Vehicle', alignRight: false },
+  { id: 'address', label: 'Address', alignRight: false },
+  { id: 'company', label: 'Company', alignRight: false },
+  { id: 'role', label: 'Role', alignRight: false },
+  { id: 'isVerified', label: 'Verified', alignRight: false },
+  { id: 'status', label: 'Status', alignRight: false },
+  { id: '' },
+];
+
+export default function UserPageDetail() {
+  const [page, setPage] = useState(0);
+
+  const [order, setOrder] = useState('asc');
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const location = useLocation();
+  const { dataSelected } = location.state;
+  const matchingUsers = [];
+
+  // matching dataSelected with USERLIST
+  dataSelected.forEach((data) => {
+    USERLIST.forEach((user) => {
+      if (user.name === data) {
+        matchingUsers.push(user);
+      }
+    });
+  });
+
+  const handleDeleteUser = () => {
+    
+  };
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  if (matchingUsers.length === 0) {
+    return <div>No users found with that name</div>;
+  }
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+  return (
+    <>
+      <Helmet>
+        <title> User | Minimal UI </title>
+      </Helmet>
+
+      <Container>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h4" gutterBottom>
+            User Detail
+          </Typography>
+        </Stack>
+
+        <Card>
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserDetailListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={USERLIST.length}
+                  onRequestSort={handleRequestSort}
+                />
+                <TableBody>
+                  {matchingUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, name, avatarUrl, gender, vechile, address, company, role, status, isVerified } = row;
+                    return (
+                      <TableRow hover key={id}>
+
+                        <TableCell component="th" scope="row" padding="normal">
+                          <Stack direction="row" alignItems="center" spacing={2}>
+                            <Avatar alt={name} src={avatarUrl} />
+                            <Typography variant="subtitle2" noWrap>
+                              {name}
+                            </Typography>
+                          </Stack>
+                        </TableCell>
+
+                        <TableCell align="left">{gender}</TableCell>
+
+                        <TableCell align="left">{vechile}</TableCell>
+
+                        <TableCell align="left">{address}</TableCell>
+
+                        <TableCell align="left">{company}</TableCell>
+
+                        <TableCell align="left">{role}</TableCell>
+
+                        <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
+
+                        <TableCell align="left">
+                          <Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
+                        </TableCell>
+
+                        <TableCell align="right">
+                          <IconButton size="large" color="inherit" onClick={handleDeleteUser(name)}>
+                            <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={USERLIST.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
+
+    </>
+  );
+}
